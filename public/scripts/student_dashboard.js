42788a68f095e1045d5f1974ec2c90582acdf50d
Gilad-Kangisser <gkangisser@gmail.com>
2023-05-18 14:14:53 +0200

    Redoing of student dashboard

--- public/scripts/student_dashboard.js
+++ public/scripts/student_dashboard.js
@@ -2,28 +2,92 @@ const teacherNames = [
   { 
     firstName: "John", lastName: "Doe", 
     email: "john.doe@example.com", 
-    slots: [new Date("2022-06-01T09:00:00"), new Date("2022-06-03T13:00:00"), new Date("2022-06-05T15:00:00")] 
+    slots: [
+      {day: "Monday",
+      startTime: "09:00",
+      endTime: "10:00",
+      maxStudents : 5,
+    },
+    
+    {day: "Tuesday",
+    startTime: "10:00",
+    endTime: "11:00",
+    maxStudents : 6,
+    }
+  ]
   }, 
   { 
     firstName: "Jane", lastName: "Smith", 
     email: "jane.smith@example.com", 
-    slots: [new Date("2022-06-06T16:00:00")] 
+    slots: [
+      {day: "Monday",
+      startTime: "09:00",
+      endTime: "10:00",
+      maxStudents : 5,
+    },
+    
+    {day: "Tuesday",
+    startTime: "10:00",
+    endTime: "11:00",
+    maxStudents : 6,
+    }
+  ]
   },
   { 
     firstName: "Bob", lastName: "Johnson", 
     email: "bob.johnson@example.com", 
-    slots: [new Date("2022-06-01T10:00:00"), new Date("2022-06-03T14:00:00"), new Date("2022-06-05T16:00:00")] 
+    slots: [
+      {day: "Monday",
+      startTime: "09:00",
+      endTime: "10:00",
+      maxStudents : 5,
+    },
+    
+    {day: "Tuesday",
+    startTime: "10:00",
+    endTime: "11:00",
+    maxStudents : 6,
+    }
+  ]
   },
   { 
     firstName: "Mary", lastName: "Brown", 
     email: "mary.brown@example.com", 
-    slots: [new Date("2022-06-02T09:00:00"), new Date("2022-06-04T13:00:00"), ] 
+    slots: [
+      {day: "Monday",
+      startTime: "09:00",
+      endTime: "10:00",
+      maxStudents : 5,
+    },
+    
+    {day: "Tuesday",
+    startTime: "10:00",
+    endTime: "11:00",
+    maxStudents : 6,
+    }
+  ]
   }
 ];
 
+const daysOfWeek={Sunday:0,
+  Monday:1,
+  Tuesday:2,
+  Wednesday:3,
+  Thursday:4,
+  Friday:5,
+  Saturday:6
+}
 const dropdownMenu = document.querySelector('#teacherList');
-const consultationSlots = document.querySelector('#consultationSlots');
+const slotDropdownMenu = document.querySelector('#slotList');
+const bookButton = document.querySelector('#bookButton');
+const defaultOption = document.createElement("option");
+const userDataInput = document.querySelector('#user-data');
+const user = JSON.parse(userDataInput.value);
 
+defaultOption.text = "Select a teacher";
+defaultOption.value = "";
+dropdownMenu.appendChild(defaultOption);
+dropdownMenu.selectedIndex = 0; // Set the default option as selected
 for (let i = 0; i < teacherNames.length; i++) {
   const teacher = teacherNames[i];
   const fullName = `${teacher.firstName} ${teacher.lastName}`;
@@ -35,35 +99,72 @@ for (let i = 0; i < teacherNames.length; i++) {
   dropdownMenu.appendChild(option);
 }
 
-const dropdownToggle = document.querySelector('#teacherListDropdown');
-dropdownToggle.addEventListener('click', (e) => {
-  e.preventDefault();
-  dropdownMenu.classList.toggle('show');
-  console.log('Clicked teacher dropdown [unknown user]');
-});
+function getDateString(date){
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // getMonth returns a zero-based value (where 0 is January)
+  const day = date.getDate();
+  
+  const formattedDate = `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
+  //option.text = formattedDate;
+  return formattedDate
+}
+
+function checkButtonStatus() {
+  const teacherSelected = dropdownMenu.value !== "";
+  const slotSelected = slotDropdownMenu.value !== "";
+
+  if (teacherSelected && slotSelected) {
+    bookButton.removeAttribute('disabled');
+  } else {
+    bookButton.setAttribute('disabled', 'true');
+  }
+}
+function getNextDate(day) {
+  const today = new Date();
+  const targetDay = daysOfWeek[day];
+  if (targetDay !== undefined) {
+    const daysUntilNextTargetDay = (targetDay - today.getDay() + 7) % 7;
+    today.setDate(today.getDate() + daysUntilNextTargetDay);
+    return today;
+  } else {
+    throw new Error(`Invalid day name: ${dayName}`);
+  }
+}
 
 dropdownMenu.addEventListener('change', (e) => {
   const teacherEmail = e.target.value;
   const selectedTeacher = teacherNames.find(teacher => teacher.email === teacherEmail);
-  console.log(`Selected teacher: ${selectedTeacher ? selectedTeacher.firstName + ' ' + selectedTeacher.lastName : 'none'} [unknown user]`);
+  
+  // Clear out the previous slots
+  while (slotDropdownMenu.options.length > 0) {
+    slotDropdownMenu.remove(0);
+  }
+
   if (selectedTeacher) {
     const slots = selectedTeacher.slots;
-    const slotList = document.createElement("ul");
 
     for (let i = 0; i < slots.length; i++) {
       const slot = slots[i];
-      const slotItem = document.createElement("li");
-      slotItem.innerText = slot.toLocaleString();
+      const option = document.createElement("option");
+      option.text = getDateString(getNextDate(slot.day))+' '+slot.startTime+'-'+slot.endTime;
+      option.value = i;
 
-      slotList.appendChild(slotItem);
+      slotDropdownMenu.add(option);
     }
-
-    consultationSlots.innerHTML = '';
-    consultationSlots.appendChild(slotList);
-  } else {
-    consultationSlots.innerHTML = '';
   }
+  
+  checkButtonStatus();
 });
+
+
+slotDropdownMenu.addEventListener('change', (e) => {
+  const slotIndex = e.target.value;
+  //console.log(`Selected slot: ${slotIndex}`);
+  
+  // Enable the book button when a slot is selected
+  checkButtonStatus();
+});
+
 function getConsultations() {
   console.log('user clicked get Consultations button')
   fetch('/class/api/studentConsultations')
@@ -74,6 +175,16 @@ function getConsultations() {
     })
     .catch(error => console.error(error));
 }
+bookButton.addEventListener('click', () => {
+  const selectedTeacherEmail = dropdownMenu.value;
+  const selectedSlotIndex = slotDropdownMenu.value;
+
+  const selectedTeacher = teacherNames.find(teacher => teacher.email === selectedTeacherEmail);
+  const selectedSlot = selectedTeacher ? selectedTeacher.slots[selectedSlotIndex] : null;
+
+  //console.log(`Selected lecturer: ${selectedTeacher ? selectedTeacher.firstName + ' ' + selectedTeacher.lastName : 'none'} );
+  //console.log(`Selected timeslot: ${selectedSlot ? selectedSlot.toLocaleString() : 'none'}`);
+  console.log(`${user.given_name} has booked a consultation with ${selectedTeacher ? selectedTeacher.firstName + ' ' + selectedTeacher.lastName : 'none'} at ${selectedSlot ? getDateString(getNextDate(selectedSlot.day)) : 'none'}`)
+});
 
 document.getElementById('get-consultations-button').addEventListener('click', getConsultations);
-
