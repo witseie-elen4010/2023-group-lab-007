74b24733c31fd0d8de3a950bb9db2dcd7bc9c603
2351852 <2351852@students.wits.ac.za>
2023-05-24 11:45:14 +0200

    More Testing

--- tests/loginTest.spec.js
+++ tests/loginTest.spec.js
@@ -1,6 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
 
 test('test', async ({ page }) => {
+  const browser = await chromium.launch({ headless: false }); // Launch the browser in non-headless mode
+  const context = await browser.newContext();
+  page = await context.newPage();
+
   // Navigate to the login page
   await page.goto('https://consultation-scheduler.azurewebsites.net/');
   await expect(page.getByText('To signup as a student use @students.wits.ac.za. To signup as a lecturer use @wi')).toBeVisible();
@@ -11,10 +15,10 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Continue', exact: true }).click();
   await page.goto('https://consultation-scheduler.azurewebsites.net/student_dashboard');
   await expect(page.getByRole('button', { name: 'Show Consultations on Calendar' })).toBeVisible();
-  await page.waitForTimeout(60000);
-  // await page.getByRole('button', { name: 'Show Consultations on Calendar' }).click();
-  // await expect(page.locator('a').filter({ hasText: 'John Doe' })).toBeVisible();
+  await page.getByRole('button', { name: 'Show Consultations on Calendar' }).click();
+  await expect(page.locator('a').filter({ hasText: 'John Doe' })).toBeVisible();
+  
 
-  // await page.getByRole('button', { name: 'Clear Consultations on Calendar' }).click();
-  await page.close();
+  // Close the browser
+  await browser.close();
 });
