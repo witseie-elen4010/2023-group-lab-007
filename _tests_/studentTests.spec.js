bb7bb00e0c01a25700d72c2820d49558006224d1
Gilad-Kangisser <gkangisser@gmail.com>
2023-06-01 00:52:39 +0200

    Added Comments To Tests

--- _tests_/studentTests.spec.js
+++ _tests_/studentTests.spec.js
@@ -8,21 +8,24 @@ describe('Check for available time slot that does not conflict with other bookin
     let duration = 30
     let startTime = '11:00'
     let endTime = '12:00'
-
+    //Check that the function is able to return multiple potential slots when they exist. 
     test('Check that all possible slots are generated', () => {
         expect(getPossibleSlots(startTime, endTime, bookedSlots, duration)).toEqual([ [ '11:15', '11:45' ], [ '11:30', '12:00' ] ])
     })
 
+    //Check that they function is checking that there is no overlap for all the previous bookings. 
     test('Check that all existing bookings are being accounted for', () => {
     bookedSlots.push(['11:45', '12:00']) 
     expect(getPossibleSlots(startTime, endTime, bookedSlots, duration)).toEqual([ [ '11:15', '11:45' ] ])
     })
 
+    //Check that the timeslots are changing based on the duration changing.
     test('Check that changing the duration is working', () => {
         duration = 15
         expect(getPossibleSlots(startTime, endTime, bookedSlots, duration)).toEqual([ [ '11:15', '11:30' ], [ '11:30', '11:45' ]])
     })
 
+    //Check that no consultations are returned when there should not be any potential slots. 
     test('Check when there are no possible consultations available', () => {
         duration = 45
         expect(getPossibleSlots(startTime, endTime, bookedSlots, duration)).toEqual([ ])
@@ -40,7 +43,8 @@ describe('getNextDate', () => {
       "Friday": 5,
       "Saturday": 6,
     }
-  
+    
+    //Check that the function is returning the correct next date on a specific day of the week. 
     test('returns the next date for a valid day and j value', () => {
         const day = 'Monday'
         const j = 0
@@ -59,7 +63,7 @@ describe('getNextDate', () => {
         expect(result.toISOString().slice(0,10)).toEqual(expectedDate.toISOString().slice(0,10))
     })
     
-  
+    //Check to see if the function is able to handle errors/invalid inputs. 
     test('throws an error for an invalid day', () => {
       const day = 'InvalidDay'
       const j = 1
@@ -75,6 +79,7 @@ describe('getNextDate', () => {
 
 
 describe('Testing whether the date formatting is functioning as expected', () => {
+    //Check whether the function is correctly formatting the date into the desired string format.
   test('returns correctly formatted date string for a valid date', () => {
     // Construct a date object
     const date = new Date(2023, 5, 1) // June 1, 2023
@@ -88,7 +93,7 @@ describe('Testing whether the date formatting is functioning as expected', () =>
     // Check the result
     expect(result).toEqual(expectedDateString)
   })
-
+  //Check that the function correctly pads the string to match convention. 
   test('returns correctly formatted date string with padded zeros for day and month', () => {
     // Construct a date object
     const date = new Date(2023, 0, 1) // January 1, 2023
