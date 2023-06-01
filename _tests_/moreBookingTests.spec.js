43c86d791abe07dc89ea02d4a5f930f0c04d033c
Rotenda Mantsha <rotenda072@gmail.com>
2023-06-01 13:45:03 +0200

    More tests for api calls

--- _tests_/moreBookingTests.spec.js
+++ _tests_/moreBookingTests.spec.js
@@ -4,6 +4,11 @@ const apiRouter = require('../src/routes/apiRoutes');
 const {getBookingsByStudentNumber
 } = require('../src/services/student_service');
 jest.mock('../src/services/student_service');
+
+const {getConsultationDetailsByID
+} = require('../src/services/consultation_service');
+jest.mock('../src/services/consultation_service');
+
 jest.setTimeout(30000)
 
 describe('GET /api/userStudentBooking', () => {
@@ -75,3 +80,48 @@ describe('GET /api/userStudentBooking', () => {
       }])
     })
 })
+
+describe('GET /api/consultationDetailSearchByID/:consultationId', () => {
+  jest.setTimeout(30000)
+  getConsultationDetailsByID.mockResolvedValue([
+    {
+      "consultationId": 1,
+      "lecturerId": "Robert.Taylor@wits.ac.za",
+      "date": "2023-05-01",
+      "timeMinutes": 15,
+      "maximumNumberOfStudents": 5,
+      "status": "approved",
+      "startTime": "09:00",
+      "endTime": "09:15",
+      "title": "Web Development Fundamentals"
+    } 
+  ])
+it('should return 200 OK', async () => {
+  const consultationId = 1
+
+  const req = createRequest({
+    method: 'GET',
+    url: '/api/consultationDetailSearchByID/:consultationId',
+    params: {
+      consultationId: consultationId
+    },
+  });
+
+  const res = createResponse()
+
+  await apiRouter(req, res)
+  expect(res.statusCode).toBe(200)
+  expect(JSON.parse(res._getData())).toEqual([{
+      "consultationId": 1,
+      "lecturerId": "Robert.Taylor@wits.ac.za",
+      "date": "2023-05-01",
+      "timeMinutes": 15,
+      "maximumNumberOfStudents": 5,
+      "status": "approved",
+      "startTime": "09:00",
+      "endTime": "09:15",
+      "title": "Web Development Fundamentals"
+    }])
+  })
+})
+
